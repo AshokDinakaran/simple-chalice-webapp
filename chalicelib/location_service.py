a010e305dfc674bdf9c02c4339109c878333df0f
Ashok <ashok.samson@gmail.com>
2023-04-24 14:35:53 +0100

    Version 1

--- chalicelib/location_service.py
+++ chalicelib/location_service.py
@@ -3,5 +3,7 @@ def information_about(place):
         return "London is the capital and largest city of England and the United Kingdom"
     if place == "Oxford":
         return "Oxford is home to the University of Oxford, the oldest university in the English-speaking world"
+    if place == "Watford":
+        return "Boaring town in Hertfordshire"
 
     return f"Unknown place: {place}"
